d6524bf7dddb54844177b49da3de8c76944457b8
Emily <emily.apitzsch@gmail.com>
2025-05-26 20:12:57 +0200

    add routes in app.py

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/')
+def search():
+    return render_template('search.html')
+
+@app.route('/')
+def city():
+    return render_template('city.html')
+
+@app.route('/')
+def review():
+    return render_template('review.html')
+
+@app.route('/')
+def user():
+    return render_template('user.html')
