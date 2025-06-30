609dc409e75187e7617acc21a400910c8421fe27
Sarah Abdulsayed <abdulsayedsarah@gmail.com>
2025-06-30 09:41:26 +0200

    Fixed image paths.

--- sql/-- SQLite.sql
+++ sql/-- SQLite.sql
@@ -1,20 +1,11 @@
 --Datei die genutzt wird um Änderungen durch SQL-Statements an der DB durchzuführen 
-DELETE FROM review WHERE id IN (1, 2);
 
-INSERT INTO review (
-    id, city_name, user_id, overall_rating, uni_rating, freetime_rating, nightime_rating,
-    campus_life_rating, transportation_rating, cost_rating, living_rating,
-    workopportunities_rating, safety_rating, food_rating, comunication_rating, comment, created_at
-) VALUES
-(
-    1, 'Berlin', 'sarah',
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    'Berlin is great for students!',
-    '2025-06-08 11:27:24'
-),
-(
-    2, 'Amsterdam', 'emmy',
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    'Beautiful city with good infrastructure.',
-    '2025-06-08 11:27:24'
-);
+UPDATE city
+SET image_path = CASE
+    WHEN name = 'Berlin' THEN 'berlin_pic.jpg'
+    WHEN name = 'Barcelona' THEN 'barcelona.jpg'
+END
+WHERE name IN ('Berlin', 'Barcelona');
+
+
+
